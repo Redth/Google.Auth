b348d695cccd1f066905d181b78093db6634fb2a
Jonathan Dick <jondick@gmail.com>
2011-06-02 15:11:11 -0400

    Added forgotten file

---
+++ Google.Auth/Exceptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Google.Auth
+{
+	public class GoogleOAuthException : Exception
+	{
+		public GoogleOAuthException(string message)
+			: base(message)
+		{
+			this.ServerResponseCode = System.Net.HttpStatusCode.Accepted;
+			this.ServerResponse = string.Empty;
+		}
+
+		public System.Net.HttpStatusCode ServerResponseCode
+		{
+			get;
+			set;
+		}
+
+		public string ServerResponse
+		{
+			get;
+			set;
+		}
+
+	}
+}
